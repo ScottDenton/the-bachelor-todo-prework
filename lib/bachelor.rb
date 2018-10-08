12451721db2c1116d2997c7876fd918f0432c971
ScottDenton <scottyd_14@hotmail.com>
2018-10-08 16:24:13 -0700

    All tests passing

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,56 @@
+require "pry"
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name = "unknown"
+  data[season].each do |people|
+
+      if people["status"] == "Winner"
+        first_name =  people["name"]
+      end
+    end
+
+  first_name.split(" ").first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do|person|
+    if person["occupation"] == occupation
+      return person["name"]
+    end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  result = 0
+  data.each do |season, contestants|
+    contestants.each do|person|
+    if person["hometown"] == hometown
+      result += 1
+    end
+    end
+  end
+  result
 end
 
 def get_occupation(data, hometown)
-  # code here
+occupations = []
+  data.each do |season, contestant|
+    contestant.each do |person|
+      if person["hometown"] == hometown
+        occupations << person["occupation"]
+    end
+  end
+end
+occupations[0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = 0
+  data[season].each do |person|
+    persons_age = person["age"].to_f
+    ages = ages + persons_age
+  end
+  average_age = (ages/ (data[season].length)).to_f.round
+average_age.to_f.round
 end
